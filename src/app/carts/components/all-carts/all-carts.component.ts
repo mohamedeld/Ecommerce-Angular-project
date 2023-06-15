15586996d62f40938159e34bc633347443cc4017
mohamedeld <mohamed.azoz20010@gmail.com>
2023-06-15 11:11:38 +0300

    amount and total price and delete product from cart

--- src/app/carts/components/all-carts/all-carts.component.ts
+++ src/app/carts/components/all-carts/all-carts.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllCartsComponent implements OnInit{
   productCarts:any[] = [];
+  total:number = 0;
   imageStyle={
     width:"100%",
     height:"5rem",
@@ -20,9 +21,35 @@ export class AllCartsComponent implements OnInit{
   getCartProduct(){
     if("cart" in localStorage){
       this.productCarts = JSON.parse(localStorage.getItem("cart")!);
-      console.log(this.productCarts)
-    }else{
-      console.log('no data')
+    }
+    this.getCartTotal()
+  }
+  getCartTotal(){
+    this.total = 0;
+    for(let i in this.productCarts){
+      this.total += this.productCarts[i].item.price * this.productCarts[i].quantity;
+    }
+  }
+  increaseAmount(index:number){
+    this.productCarts[index].quantity++;
+    this.getCartTotal()
+    localStorage.setItem("cart",JSON.stringify(this.productCarts))
+  }
+  decreaseAmount(index:number){
+    this.productCarts[index].quantity--;
+    this.getCartTotal()
+    localStorage.setItem("cart",JSON.stringify(this.productCarts))
+  }
+  detectChange(){
+    localStorage.setItem("cart",JSON.stringify(this.productCarts))
+  }
+  deleteProduct(index:number){
+    let confirmData = confirm("you want to delete")
+    if(confirmData === true){
+      this.productCarts.splice(index,1);
+      localStorage.setItem("cart",JSON.stringify(this.productCarts))
+    } else{
+      return;
     }
   }
 }
